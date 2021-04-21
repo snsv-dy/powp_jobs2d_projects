1f298b47b66954103124d187cc769e023964f634
BartekSwistak99 <bartekswistak20@gmail.com>
2021-04-21 13:30:07 +0200

    Monitoring device test - initial bug fixed

--- src/test/java/edu/kis/powp/jobs2d/events/SelectMonitoringDeviceTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectMonitoringDeviceTestFigureOptionListener.java
@@ -1,8 +1,6 @@
 package edu.kis.powp.jobs2d.events;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.command.OperateToCommand;
-import edu.kis.powp.jobs2d.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.UsageMonitoringDriver;
 
@@ -11,31 +9,37 @@ import java.awt.event.ActionListener;
 
 public class SelectMonitoringDeviceTestFigureOptionListener implements ActionListener {
 	private final DriverManager driverManager;
+	private final int TESTING_OP_DISTANCE = 200;
+	private final int TESTING_HEAD_DISTANCE = 400;
+
+	private int initOpDistance = 0;
+	private int initHeadDistance = 0;
 
 	public SelectMonitoringDeviceTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Job2dDriver driver = driverManager.getCurrentDriver();
-		new SetPositionCommand(100, 0)
-				.execute(driver);
-		new OperateToCommand(100, 50)
-				.execute(driver);
-		new OperateToCommand(150, 50)
-				.execute(driver);
-		new OperateToCommand(150, 0)
-				.execute(driver);
-		new OperateToCommand(100, 0)
-				.execute(driver);
-		new SetPositionCommand(0, 0)
-				.execute(driver);
+		driver.setPosition(0, 0);
+		if (driver instanceof UsageMonitoringDriver) {
+			initOpDistance = ((UsageMonitoringDriver) driver).getOpDistance();
+			initHeadDistance = ((UsageMonitoringDriver) driver).getHeadDistance();
+		}
+		driver.setPosition(100, 0);
+		driver.operateTo(100, 50);
+		driver.operateTo(150, 50);
+		driver.operateTo(150, 0);
+		driver.operateTo(100, 0);
+		driver.setPosition(0, 0);
+
 		if (driver instanceof UsageMonitoringDriver) {
 			UsageMonitoringDriver usageMonitoringDriver = (UsageMonitoringDriver) driver;
-			if (usageMonitoringDriver.getOpDistance() != 200 || usageMonitoringDriver.getHeadDistance() != 400) {
+			if (usageMonitoringDriver.getOpDistance() - initOpDistance != TESTING_OP_DISTANCE ||
+					usageMonitoringDriver.getHeadDistance() - initHeadDistance != TESTING_HEAD_DISTANCE) {
 				usageMonitoringDriver.getLogger().warning("Usage monitoring test failed!");
-			}
-			else{
+			} else {
 				usageMonitoringDriver.getLogger().info("Usage monitoring test passed!");
 			}
 		}
