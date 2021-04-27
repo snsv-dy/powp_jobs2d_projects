875a6cda3b69a73f2222aadfdb9bc71fd0c7a7c4
adriankucharski <adrian1998.25@live.com>
2021-04-27 11:22:53 +0200

    merged changes, removed ComplexDriverCommand

--- src/main/java/edu/kis/powp/jobs2d/command/manager/DriverCommandManager.java
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/DriverCommandManager.java
@@ -1,13 +1,9 @@
 package edu.kis.powp.jobs2d.command.manager;
 
-import java.util.Iterator;
 import java.util.List;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.command.CompoundCommand;
 import edu.kis.powp.jobs2d.command.DriverCommand;
-import edu.kis.powp.jobs2d.command.ICompoundCommand;
-import edu.kis.powp.jobs2d.visitor.ICommandVisitor;
 import edu.kis.powp.observer.Publisher;
 
 /**
