a2ce36e4aec41f9c567326f5975c8f78d357e189
majewskimateusz <kontakt@mateuszmajewski.com>
2021-05-12 12:12:57 +0200

    Returning commands instead of executing

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
@@ -1,25 +1,21 @@
 package edu.kis.powp.jobs2d.command;
 
-import edu.kis.powp.jobs2d.features.DriverFeature;
-
 public class ComplexCommandFactory {
-    public static void drawRectangle(int x, int y, int off1, int off2) {
-        new ComplexCommand.Builder()
-                .add(new SetPositionCommand(x, y)).add(new OperateToCommand(x + off1, y))
-                .add(new OperateToCommand(x + off1, y + off2))
-                .add(new OperateToCommand(x, y + off2))
+    public ComplexCommand drawRectangle(int x, int y, int off1, int off2) {
+        return new ComplexCommand.Builder()
+                .add(new SetPositionCommand(x, y)).add(new OperateToCommand(x+off1, y))
+                .add(new OperateToCommand(x+off1, y+off2))
+                .add(new OperateToCommand(x, y+off2))
                 .add(new OperateToCommand(x, y))
-                .build()
-                .execute(DriverFeature.getDriverManager().getCurrentDriver());
+                .build();
     }
 
-    public static void drawTriangle(int x, int y, int off1, int off2) {
-        new ComplexCommand.Builder()
+    public ComplexCommand drawTriangle(int x, int y, int off1, int off2) {
+        return new ComplexCommand.Builder()
                 .add(new SetPositionCommand(x, y))
-                .add(new OperateToCommand(x, y + off1))
-                .add(new OperateToCommand(x + off2, y + off1))
+                .add(new OperateToCommand(x, y+off1))
+                .add(new OperateToCommand(x+off2, y+off1))
                 .add(new OperateToCommand(x, y))
-                .build()
-                .execute(DriverFeature.getDriverManager().getCurrentDriver());
+                .build();
     }
 }
