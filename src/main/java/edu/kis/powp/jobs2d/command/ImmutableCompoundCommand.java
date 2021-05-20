4a0376a246688753288c6e136d74e79a4f913f7e
adriankucharski <adrian1998.25@live.com>
2021-05-20 23:35:15 +0200

    implementation of immutable ICompoundCommand

--- src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
@@ -1,10 +1,48 @@
 package edu.kis.powp.jobs2d.command;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
 
-public final class ImmutableCompoundCommand extends CompoundCommand{
+public final class ImmutableCompoundCommand implements ICompoundCommand {
+    private final CompoundCommand compoundCommand;
 
+    public ImmutableCompoundCommand(CompoundCommand compound){
+        compoundCommand = compound.clone();
+    }
+    public ImmutableCompoundCommand(List<DriverCommand> driverCommands, String name){
+        compoundCommand = new CompoundCommand(driverCommands, name).clone();
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        compoundCommand.execute(driver);
+    }
+
+    @Override
+    public DriverCommand clone() {
+        return new ImmutableCompoundCommand(compoundCommand);
+    }
+
+    @Override
+    public Iterator<DriverCommand> iterator() {
+        return compoundCommand.clone().iterator();
+    }
+
+    @Override
+	public boolean equals(Object o) {
+        return compoundCommand.equals(o);
+    }
+
+    @Override
+	public String toString() {
+		return compoundCommand.toString();
+	}
+
+    public ImmutableCompoundCommand addCommand(DriverCommand command){
+        ImmutableCompoundCommand obj = (ImmutableCompoundCommand) this.clone();
+        obj.compoundCommand.addCommand(command);
+        return obj;
+    }
 }
