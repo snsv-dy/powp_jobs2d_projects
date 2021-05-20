d9648b2a6252c234d9dc55b6c0bc372eaa9b7a63
adriankucharski <adrian1998.25@live.com>
2021-05-20 22:59:45 +0200

    immutable init

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
@@ -0,0 +1,10 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public final class ImmutableCompoundCommand extends CompoundCommand{
+
+}
