69960dd303e36f7bdac57f2c0b4172fffaefcc3b
BartekSwistak99 <bartekswistak20@gmail.com>
2021-06-15 18:25:16 +0200

    spaces changed to tabs

--- src/test/java/edu/kis/powp/jobs2d/CompositeDriverTest.java
+++ src/test/java/edu/kis/powp/jobs2d/CompositeDriverTest.java
@@ -5,79 +5,80 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CompositeDriverTest {
-    private Job2dDriver sampleDriver = new SampleDriver();
-    private Job2dDriver testDriver = new TestDriver();
-    private CompositeDriver compositeDriver = new CompositeDriver();
-    @BeforeEach
-    void setUp(){
-        compositeDriver.add(sampleDriver);
-        compositeDriver.add(testDriver);
-    }
+	private Job2dDriver sampleDriver = new SampleDriver();
+	private Job2dDriver testDriver = new TestDriver();
+	private CompositeDriver compositeDriver = new CompositeDriver();
 
-    @Test
-    void setToTest(){
-        compositeDriver.setPosition(1, 2);
-    }
+	@BeforeEach
+	void setUp() {
+		compositeDriver.add(sampleDriver);
+		compositeDriver.add(testDriver);
+	}
 
-    @Test
-    void operateToTest(){
-        compositeDriver.operateTo(1, 10);
-    }
+	@Test
+	void setToTest() {
+		compositeDriver.setPosition(1, 2);
+	}
 
-    @Test
-    void checkSizeOfChildrenList(){
-        assertEquals(2, compositeDriver.getChildren().length);
-        compositeDriver.remove(sampleDriver);
-        assertEquals(1, compositeDriver.getChildren().length);
-    }
-    @Test
-    void getChildrenTest(){
-        Job2dDriver[] array = compositeDriver.getChildren();
-        assertTrue(Arrays.stream(array).anyMatch(x-> x.equals(sampleDriver))
-                && Arrays.stream(array).anyMatch(x-> x.equals(testDriver)));
-    }
+	@Test
+	void operateToTest() {
+		compositeDriver.operateTo(1, 10);
+	}
 
-    @Test
-    void booleanHandlerTest(){
-        SampleDriver sampleDriver2 = new SampleDriver();
-        compositeDriver.checkBoxHandler(sampleDriver2);
-        assertEquals(3, compositeDriver.getSize());
-        compositeDriver.checkBoxHandler(sampleDriver2);
-        assertEquals(2, compositeDriver.getSize());
+	@Test
+	void checkSizeOfChildrenList() {
+		assertEquals(2, compositeDriver.getChildren().length);
+		compositeDriver.remove(sampleDriver);
+		assertEquals(1, compositeDriver.getChildren().length);
+	}
 
-    }
+	@Test
+	void getChildrenTest() {
+		Job2dDriver[] array = compositeDriver.getChildren();
+		assertTrue(Arrays.stream(array).anyMatch(x -> x.equals(sampleDriver))
+				&& Arrays.stream(array).anyMatch(x -> x.equals(testDriver)));
+	}
+
+	@Test
+	void booleanHandlerTest() {
+		SampleDriver sampleDriver2 = new SampleDriver();
+		compositeDriver.checkBoxHandler(sampleDriver2);
+		assertEquals(3, compositeDriver.getSize());
+		compositeDriver.checkBoxHandler(sampleDriver2);
+		assertEquals(2, compositeDriver.getSize());
+
+	}
 
 
+	private static class SampleDriver implements Job2dDriver {
 
-    private static class SampleDriver implements Job2dDriver{
+		@Override
+		public void setPosition(int i, int i1) {
+			System.out.println("Sample Driver set position at pos = " + i + ", " + i1);
+		}
 
-        @Override
-        public void setPosition(int i, int i1) {
-            System.out.println("Sample Driver set position at pos = " + i + ", " + i1);
-        }
+		@Override
+		public void operateTo(int i, int i1) {
+			System.out.println("Sample driver operate to pos = " + i + ", " + i1);
 
-        @Override
-        public void operateTo(int i, int i1) {
-            System.out.println("Sample driver operate to pos = " + i + ", " + i1);
+		}
+	}
 
-        }
-    }
+	private static class TestDriver implements Job2dDriver {
 
-    private static class TestDriver implements Job2dDriver{
+		@Override
+		public void setPosition(int i, int i1) {
+			System.out.println("Test driver set position at pos = " + i + ", " + i1);
+		}
 
-        @Override
-        public void setPosition(int i, int i1) {
-            System.out.println("Test driver set position at pos = " + i + ", " + i1);
-        }
-
-        @Override
-        public void operateTo(int i, int i1) {
-            System.out.println("Test driver operate to pos = " + i + ", " + i1);
-        }
-    }
+		@Override
+		public void operateTo(int i, int i1) {
+			System.out.println("Test driver operate to pos = " + i + ", " + i1);
+		}
+	}
 }
