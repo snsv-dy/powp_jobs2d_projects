9f17c70bc48c5f67328d6d1e1c40e8ef0bc59d92
adriankucharski <adrian1998.25@live.com>
2021-06-15 19:13:43 +0200

    Spaces to tabs

--- src/main/java/edu/kis/powp/jobs2d/features/DriverUtilsFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DriverUtilsFeature.java
@@ -11,22 +11,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class DriverUtilsFeature implements Feature{
-    private Application app;
+public class DriverUtilsFeature implements Feature {
+	private Application app;
 
-    @Override
-    public void setup(Application application) {
-        app = application;
-        app.addComponentMenu(DriverUtilsFeature.class, "Drivers utils");
-    }
+	@Override
+	public void setup(Application application) {
+		app = application;
+		app.addComponentMenu(DriverUtilsFeature.class, "Drivers utils");
+	}
 
-    public void addUtilsCheckBox(String name, Job2dDriver driver, DriverManager driverManager) {
-        ActionListener listener = (ActionEvent e)->{
-            if(((JCheckBoxMenuItem)e.getSource()).getState())
-                driverManager.addUtilsDriver(driver);
-            else
-                driverManager.removeUtilsDriver(driver);
-        };
-        app.addComponentMenuElementWithCheckBox(DriverUtilsFeature.class, name, listener, false);
-    }
+	public void addUtilsCheckBox(String name, Job2dDriver driver, DriverManager driverManager) {
+		ActionListener listener = (ActionEvent e) -> {
+			if (((JCheckBoxMenuItem) e.getSource()).getState())
+				driverManager.addUtilsDriver(driver);
+			else
+				driverManager.removeUtilsDriver(driver);
+		};
+		app.addComponentMenuElementWithCheckBox(DriverUtilsFeature.class, name, listener, false);
+	}
 }
